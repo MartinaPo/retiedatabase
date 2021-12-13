39c0df1b002b790340559cafbba599f6c67115a6
Martina <94382770+MartinaPo@users.noreply.github.com>
2021-12-13 19:28:22 +0100

    Add files via upload

--- query_sql.sql
+++ query_sql.sql
@@ -1,133 +1,200 @@
-/*query varie per i test*/
-
 SELECT * FROM docenti;
 SELECT * FROM materie;
 
-
-SELECT * FROM materie
- JOIN docenti ON materie.docente_id = docenti.id;
-
 SELECT materie.nome, materie.ore, docenti.cognome, docenti.nome
- FROM materie
- JOIN docenti ON materie.docente_id = docenti.id;
+    FROM docenti 
+    JOIN materie ON materie.docente_id = docenti.id;
+
+    
+SELECT materie.*, docenti.cognome
+    FROM materie 
+    JOIN docenti ON docente_id = docenti.id;
 
 
-SELECT cognome
- FROM materie
- JOIN docenti ON materie.docente_id = docenti.id;
-
-SELECT * FROM frequenze
- JOIN alunni ON frequenze.alunno_id = alunni.id;
-
-
-
-------------------------------------------------------------------------
+SELECT alunni.cognome, alunni.nome, materie.nome, docenti.cognome, frequenze.media, frequenze.ore_assenza
+FROM frequenze
+JOIN alunni  ON frequenze.alunno_id = alunni.id
+JOIN materie ON frequenze.materia_id = materie.id
+JOIN docenti ON materie.docente_id = docenti.id
+/* WHERE */
+ORDER BY frequenze.id DESC, docenti.cognome ASC;
 
 
- /*query che unisca le frequenze (alunni)*/
-
- SELECT alunni.nome, alunni.cognome, frequenze.*
- FROM frequenze
- JOIN alunni ON frequenze.alunno_id = alunni.id;
-
-
-SELECT alunni.nome, alunni.cognome, materie.nome, frequenze.media, frequenze.ore_assenza, docenti.cognome
- FROM frequenze
- JOIN alunni ON frequenze.alunno_id = alunni.id
- JOIN materie ON frequenze.materia_id = materie.id
- JOIN docenti ON materie.docente_id = docenti.id;
-
-/*LEFT o RIGHT OUTER JOIN*/
+/* LEFT OUTER JOIN - RIGHT OUTER JOIN */
 SELECT *
- FROM frequenze /*INNER*/ JOIN alunni ON frequenze.alunno_id = alunni.id;
+FROM frequenze /* INNER */ JOIN alunni ON frequenze.alunno_id = alunni.id;
+
+SELECT *
+FROM frequenze LEFT OUTER JOIN alunni ON frequenze.alunno_id = alunni.id;
 
 SELECT *
 FROM frequenze RIGHT OUTER JOIN alunni ON frequenze.alunno_id = alunni.id;
 
+
+/* WHERE */
 SELECT *
- FROM frequenze LEFT OUTER JOIN alunni ON frequenze.alunno_id = alunni.id;
-
----------------------------------------------------------------
-
-/*nome e cognome dei docenti di fascia A in qualche materia*/
-
-SELECT docenti.nome, docenti.cognome, materie.nome, materie.docente_fascia
- FROM docenti
- JOIN docenti ON materie.docente_id = docenti.id
- WHERE materie.docente_fascia = 'A';
+FROM docenti
+WHERE nome = 'Matteo' AND cognome = 'Arosti';
 
 
-/*tutte le materie ordinate per anno, numero ore DESC e nome*/
+SELECT *
+FROM docenti
+WHERE nome = 'Matteo' AND cognome = 'Ribuoli';
 
 
-SELECT * FROM materie
- ORDER BY anno, materie.ore DESC, materie.nome;
+SELECT *
+FROM docenti
+WHERE nome = 'Matteo' OR cognome = 'Ribuoli';
+
+SELECT *
+FROM frequenze 
+    LEFT OUTER JOIN alunni ON frequenze.alunno_id = alunni.id
+WHERE alunni.id = 1 AND (alunni.nome = 'Martina' OR alunni.nome = 'Francesca');
+
+SELECT *
+FROM frequenze 
+    LEFT OUTER JOIN alunni ON frequenze.alunno_id = alunni.id
+WHERE alunni.id = 1 AND alunni.nome = 'Martina' OR alunni.nome = 'Francesca';
 
 
-/*tutti i docenti che insegnano materie del primo e secondo anno*/
 
-SELECT DISTINCT docenti.id, docenti.nome, docenti.cognome
- FROM docenti
- RIGHT OUTER JOIN materie ON materie.docenti_id = docenti.id
- /*WHERE materie.anno = 1 OR materie.anno = 2;*/
- WHERE anno IN(1,2);
-
-/*tutti i docenti che insegnano materie con più di 30 ore*/
-
-SELECT DISTINCT docenti.id, docenti.nome, docenti.cognome
- FROM docenti
- JOIN materie ON materie.docenti_id = docenti.id
- WHERE materie.ore > 30;
+SELECT <nome campi>
+FROM   <tabella> EVENTUALI VARI JOIN
+WHERE  <condizioni>
+GROUP BY <nomi campi>
+HAVING <condizioni su conteggi group by >
+ORDER BY <campi ordinamento>
 
 
-/*tutti gli alunni che frequentano materie con più di 30 ore*/
-
-SELECT DISTINCT alunni.nome, alunni.cognome
- FROM alunni
- JOIN frequenze ON alunno_id = frequenze.alunno_id
- JOIN materie ON frequenze.materia_id = materie.id
- WHERE materie.ore > 30;
 
 
-/*tutti gli alunni che frequentano almeno una materia di arosti*/
 
-SELECT DISTINCT alunni.id, alunni.nome, alunni.cognome
- FROM alunni
- JOIN frequenze ON frequenze.alunno_id = alunni.id
- JOIN materie ON frequenze.materia_id = materie.id
- JOIN docenti ON frequenze.materia_id = docenti.id
- WHERE docenti.cognome = 'Arosti';
+SELECT alunni.cognome, alunni.nome, materie.nome, docenti.cognome, frequenze.media, frequenze.ore_assenza
+FROM frequenze
+JOIN alunni  ON frequenze.alunno_id = alunni.id
+JOIN materie ON frequenze.materia_id = materie.id
+JOIN docenti ON materie.docente_id = docenti.id
+WHERE frequenze.media > 7.5 AND materie.nome = 'Sistemistica' AND docenti.id < 50;
 
-
----------------------------------------
-
-/*ESEMPIO GROUP BY*/
-
-SELECT * 
- FROM materie JOIN docenti ON materie.docente_id = docenti.id;
-
-
-SELECT docenti.id, docenti.cognome, docenti.nome,
-        COUNT(*), SUM(materie.ore), AVG(materie.ore)
- FROM materie JOIN docenti ON materie.docente_id = docenti.id
- GROUP BY docenti.id, docenti.cognome, docenti.nome;
+=   -> uguale
+<>  -> diverso da
+>   -> maggiore di
+>=  -> maggiore o uguale di
+<   -> minore di
+<=  -> minore o uguale di
+IS NULL     (WHERE docenti.id IS NULL)
+IS NOT NULL
+IN  es: IN(2, 3)    oppure IN('Matteo', 'Andrea')
+NOT IN (2, 3)       oppure NOT IN('Matteo', 'Andrea')
+prezzo >= 50 AND prezzo <= 100
 
 
-SELECT alunni.id, alunni.nome, alunni.cognome 
- FROM alunni 
- JOIN frequenze ON frequenze.alunno_id = alunni.id
- GROUP BY alunni.id, alunni.nome, alunni.cognome;
 
 
-SELECT alunni.id, alunni.nome, alunni.cognome,
-    COUNT(*) AS materie_frequentate, 
+/* lista frequenze con anomalia su legame alunno (id alunno non trovato in alunni) */
+SELECT *
+FROM frequenze
+LEFT OUTER JOIN alunni ON frequenze.alunno_id = alunni.id
+WHERE alunni.id IS NULL;
+
+
+/* lista degli alunni che NON frequentano nessuna materia */
+SELECT *
+FROM frequenze RIGHT OUTER JOIN alunni ON frequenze.alunno_id = alunni.id
+WHERE frequenze.id IS NULL;
+
+
+select materie.nome, docenti.nome, docenti.cognome, materie.docente_fascia
+FROM materie
+JOIN docenti ON materie.docente_id = docenti.id
+WHERE materie.docente_fascia = 'A';
+
+
+/* FUNZIONE DISTINCT per mostrare una volta solo i record duplicati nel risultato */
+SELECT DISTINCT docenti.nome, docenti.cognome
+FROM materie
+JOIN docenti ON materie.docente_id = docenti.id
+WHERE materie.docente_fascia = 'B';
+
+
+
+1) tutte le materie ordinate per anno, numero ore (descendente), nome
+2) tutti i docenti che insegnano materie del 1° e 2° anno
+3) tutti i docenti che insegnano materie con più di 30 ore
+4) tutti gli alunni che frequentano materie con più di 30 ore
+5) tutti gli alunni che frequentano una materia di Arosti
+
+    alunni (select nome, cognome)
+    frequenze
+    materie
+    docenti (WHERE cognome='Arosti')
+
+    SELECT DISTINCT alunni.id, alunni.cognome, alunni.nome
+    FROM alunni
+    JOIN frequenze ON frequenze.alunno_id = alunni.id
+    JOIN materie   ON frequenze.materia_id = materie.id
+    JOIN docenti   ON materie.docente_id = docenti.id
+    WHERE docenti.cognome = 'Arosti';
+
+
+
+
+1)  select * from materie 
+    order by anno, materie.ore DESC, materie.nome;
+
+2)  SELECT DISTINCT docenti.id, docenti.nome, cognome
+    FROM docenti
+    JOIN materie ON materie.docente_id = docenti.id
+    /* WHERE materie.anno = 1 OR materie.anno = 2 */
+    WHERE anno IN(1, 2);
+
+3)  SELECT DISTINCT docenti.cognome, docenti.nome
+    FROM docenti
+    JOIN materie ON materie.docente_id = docenti.id
+    WHERE materie.ore > 30;
+
+4)  SELECT DISTINCT alunni.cognome, alunni.nome
+    FROM alunni
+    JOIN frequenze ON alunni.id = frequenze.alunno_id
+    JOIN materie   ON frequenze.materia_id = materie.id
+    WHERE materie.ore > 30;
+
+    SELECT *
+    FROM alunni
+    JOIN frequenze 
+        ON alunni.id = frequenze.alunno_id
+       AND alunni.pippo = frequenze.ciao
+    
+
+
+
+/* ESEMPI GROUP BY */
+SELECT *
+FROM materie JOIN docenti ON materie.docente_id = docenti.id;
+
+SELECT docenti.id, docenti.cognome, docenti.nome, 
+    COUNT(*), SUM(materie.ore), AVG(materie.ore)
+FROM materie JOIN docenti ON materie.docente_id = docenti.id
+GROUP BY docenti.id, docenti.cognome, docenti.nome;
+
+
+/* Elenco degli alunni, conteggio materie frequentate, e media della loro media */
+SELECT alunni.cognome AS pippo, 
+    alunni.nome, 
+    count(*) AS num_freq, 
     AVG(frequenze.media) AS media, 
-    SUM(ore_assenza) AS tot_ore_assenza,
-    MAX(ore_assenza) AS ore_assenza_max_per_materia,
-    MIN(ore_assenza) AS ore_assenza_min_per_materia
- FROM alunni 
- JOIN frequenze ON frequenze.alunno_id = alunni.id
- WHERE frequenze.ore_assenza > 0
- GROUP BY alunni.id, alunni.nome, alunni.cognome
- HAVING COUNT(*) > 1
- ORDER BY AVG(frequenze.media) DESC;
+    SUM(ore_assenza) AS tot_ore_ass,
+    MAX(ore_assenza) AS ore_ass_max_per_mat,
+    MIN(ore_assenza) AS ore_ass_mini_per_mat
+FROM alunni
+JOIN frequenze ON frequenze.alunno_id = alunni.id
+WHERE frequenze.ore_assenza > 0 /* filtra in origine i dati partendo dai record */
+GROUP BY alunni.id, alunni.nome, alunni.cognome
+HAVING COUNT(*) > 1             /* filtra i dati partendo dai dati di group by */
+ORDER BY AVG(frequenze.media) DESC;
+
+
+CLIENTE A ---- > 55.000€
+CLIENTE B ---- > 38.000€
+
+having sum(importo_ordine) > 50.000
